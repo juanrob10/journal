4dac5b63918f8c0ae5b8217a8aafc10b951c76bd
Juan <juanrobcode@gmail.com>
2023-06-04 09:01:04 -0700

    database configuration

--- project/settings.py
+++ project/settings.py
@@ -75,8 +75,19 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+
+        'NAME': 'mydb',
+
+        'USER': 'rob',
+
+        'PASSWORD': 'mypass',
+
+        'HOST': 'localhost',
+
+        'PORT': '5432',
+
     }
 }
 
